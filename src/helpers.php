ee66ea553c539b5daaf803992c916d5cc2465e9a
Nick de Kruijk <git@nickdekruijk.nl>
2021-04-28 16:03:47 +0200

    Add asset_resized_encode helper

--- src/helpers.php
+++ src/helpers.php
@@ -16,3 +16,21 @@ if (!function_exists('asset_resized')) {
         }
     }
 }
+
+if (!function_exists('asset_resized_encode')) {
+    /**
+     * Generate an resized urlencoded asset path for the application.
+     *
+     * @param  string  $template
+     * @param  string  $path
+     * @param  bool|null  $secure
+     * @return string
+     */
+    function asset_resized_encode($template, $path, $secure = null)
+    {
+        if ($path) {
+            $path = str_replace('%2F', '/', rawurlencode($path));
+            return asset(config('imageresize.route') . '/' . $template . '/' . $path, $secure);
+        }
+    }
+}
